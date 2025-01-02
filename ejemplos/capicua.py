e74c37c51f197319e587622dc9df3bb27afa65c5
Martin Rene <martinvilu@gmail.com>
2025-01-02 09:50:15 -0300

    mejoras en el ejemplo

--- ejemplos/capicua.py
+++ ejemplos/capicua.py
@@ -1,15 +1,20 @@
-
-cadena = input() #"neuquen"
-
+cadena = "neuq3uen" # reemplazar por un input
 i = 0
 capicua = True
-while i < (len(cadena)//2) and capicua:
-    j = len(cadena) - i - 1
-    j = -len(cadena) + i # alternativo
-    capicua = capicua and (cadena[i] == cadena[j])
-    i = i + 1
+largo = len(cadena)
+while i < (largo // 2) and capicua:
+	j = largo - i - 1
+	#j = -len(cadena) + i # alternativo
+	capicua = capicua and (cadena[i] == cadena[j])
+	i = i + 1
 
-print(capicua)
+if not capicua:
+    print(cadena)
+    print(("-"*(i-1))+"^")
+else:
+    print(f"Es capicua")
+
+
 
 
 #    print(f"{i} {j} {cadena[i]}")
